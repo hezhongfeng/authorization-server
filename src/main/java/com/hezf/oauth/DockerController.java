9c4f0c97478699df8fe5cd94ed6f4c08f71b9388
hezhongfeng <287645587@qq.com>
2023-07-27 14:45:08 +0800

    ✨ feat(core): 去除环境变量

--- src/main/java/com/hezf/oauth/DockerController.java
+++ src/main/java/com/hezf/oauth/DockerController.java
@@ -1,17 +1,17 @@
 package com.hezf.oauth;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
 
-  @Value("${DB_HOST}")
-  private String rabbitmqHost;
+  // @Value("${DB_HOST}")
+  // private String rabbitmqHost;
 
   @RequestMapping("/")
   public String index() {
-    return "Hello Index!" + rabbitmqHost;
+    return "Hello Index!";
   }
 }
