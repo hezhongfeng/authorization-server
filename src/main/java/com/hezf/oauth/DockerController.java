e4dd2f5a797e0c4055e43d826e3c7319c4b2fed0
hezhongfeng <287645587@qq.com>
2023-07-27 10:35:32 +0800

    ✨ feat(core): 测试获取env variable

--- src/main/java/com/hezf/oauth/DockerController.java
+++ src/main/java/com/hezf/oauth/DockerController.java
@@ -1,13 +1,17 @@
 package com.hezf.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
 
+  @Value("${DB_HOST}")
+  private String rabbitmqHost;
+
   @RequestMapping("/")
   public String index() {
-    return "Hello Index!";
+    return "Hello Index!" + rabbitmqHost;
   }
 }
