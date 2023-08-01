12ea7c1045421b884baf6cebf8327bd34890813e
hezhongfeng <287645587@qq.com>
2023-08-01 09:27:03 +0800

    📃 docs(client): 增加注释

--- src/test/java/com/hezf/oauth/OauthApplicationTests.java
+++ src/test/java/com/hezf/oauth/OauthApplicationTests.java
@@ -41,10 +41,11 @@ class OauthApplicationTests {
 	@Rollback(false)
 	void addClient() {
 		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-				.clientName("测试client").clientId("messaging-client")
+				.clientName("测试form").clientId("form-client")
 				// 注意存完的时候是{noop}secret，但是验证过之后就变为了{bcrypt}$2a$10$5igAFJvkf0wg.f5ml2bBgOO.13LmzgOhWwiwZZtTKCjkX0f3wiwJ2
 				// {bcrypt}$2a$10$kYEqs8S4mwUSP7ures8ZSuqeng0HI28moJ7htsXcxr3U3QtL31FAC
-				.clientSecret("{noop}secret").clientIdIssuedAt(Instant.now())
+				// 去除了{bcrypt}之后可以正常验证 new BCryptPasswordEncoder().matches("secret","$2a$10$kYEqs8S4mwUSP7ures8ZSuqeng0HI28moJ7htsXcxr3U3QtL31FAC")
+				.clientSecret("secret").clientIdIssuedAt(Instant.now())
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
